c8873e896041ea6523a8593dd1fcd43b3aa55aca
heySanj <saprasad92@gmail.com>
2024-01-10 13:50:55 +1000

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: "Sora, sans-serif",
+      serif: "Zilla Slab, serif",
+      mono: "Space Mono, monospace",
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: true,
+    darkTheme: "night",
+  },
+};
