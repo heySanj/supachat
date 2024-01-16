f04f1e5683e5ce8faea292155d997bc51510f8b6
heySanj <saprasad92@gmail.com>
2024-01-16 16:19:33 +1000

    Type fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import Chat from "./components/Chat";
 import Login from "./components/Login";
-import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 function App() {
-  const [animationParent] = useAutoAnimate({ duration: 200 });
   return (
     <>
       <ToastContainer />
@@ -14,7 +12,7 @@ function App() {
         SupaChat
       </h1>
       <Chat />
-      <Login ref={animationParent} />
+      <Login />
     </>
   );
 }
